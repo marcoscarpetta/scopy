cc21e18361fb57d21e154813d968ee035620eada
Marco Scarpetta <marcoscarpetta@mailoo.org>
2013-02-19 13:03:25 +0100

    added number of players chioce

--- src/Actions/Start.py
+++ src/Actions/Start.py
@@ -25,53 +25,56 @@ from libscopyUI import base
 
 Path=_('File')
 Name=_('New game...')
+
+win_inizio = Gtk.Window()
+win_inizio.set_title(_('New game...'))
+win_inizio.set_border_width(10)
+win_inizio.connect('delete-event', base.hide)
+table=Gtk.Table(2,4,False)
+table.attach(Gtk.Label(_('Name:')),0,1,0,1,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
+table.attach(Gtk.Label(_('Variant:')),0,1,1,2,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
+table.attach(Gtk.Label(_('Number of players:')),0,1,2,3,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
+table.set_row_spacings(5)
+table.set_col_spacings(5)
+name_entry=Gtk.Entry()
+var_combo=Gtk.ComboBoxText()
+for variante in base.varianti:
+	var_combo.append(variante,variante)
+n_players=Gtk.ComboBoxText()
+table.attach(name_entry,1,2,0,1,Gtk.AttachOptions(4),Gtk.AttachOptions(4))
+table.attach(var_combo,1,2,1,2,Gtk.AttachOptions(4),Gtk.AttachOptions(4))
+table.attach(n_players,1,2,2,3,Gtk.AttachOptions(4),Gtk.AttachOptions(4))
+button=Gtk.Button()
+button.set_label(_('Start game!'))
+table.attach(button,1,2,3,4,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
+win_inizio.add(table)
+name_entry.set_property("activates-default",True)
+button.set_can_default(True)
+button.grab_default()
 	
-def inizia_partita(widget,app,name_entry,var_combo,win_inizio):
-		base.settings['nome']=name_entry.get_text()
-		base.settings['variante']=base.varianti[var_combo.get_active()]
-		'''for obj in app.carte.carte:
-			obj.object.destroy()
-		for box in app.boxes:
-			app.boxes[box].clean()
-		app.carte = Carte()'''
-		base.settings.save()
-		win_inizio.destroy()
-		app.crea_partita()
-		#app.dichiara(app.partita.dai_carte())
-		#app.sposta_carte()
-		app.partita.start()
+def set_n_players(widget=None):
+	n_players.remove_all()
+	for n in base.get_number_of_players(var_combo.get_active_id()):
+		n_players.append(str(n),str(n))
+	try:
+		n_players.set_active_id(base.settings['players'])
+	except:
+		pass
+
+def inizia_partita(widget,app):
+	base.settings['nome']=name_entry.get_text()
+	base.settings['variante']=var_combo.get_active_id()
+	base.settings['players']=n_players.get_active_id()
+	base.settings.save()
+	win_inizio.hide()
+	app.nuova_partita()
 
 def main(widget, app):
-	win_inizio = Gtk.Window()
-	win_inizio.set_title(_('New game...'))
 	win_inizio.set_transient_for(app.window)
 	win_inizio.set_position(Gtk.WindowPosition.CENTER_ON_PARENT)
-	win_inizio.set_border_width(10)
-	win_inizio.connect('delete-event', base.destroy)
-	table=Gtk.Table(2,3,False)
-	table.attach(Gtk.Label(_('Name:')),0,1,0,1,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
-	table.attach(Gtk.Label(_('Variant:')),0,1,1,2,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
-	table.set_row_spacings(5)
-	table.set_col_spacings(5)
-	name_entry=Gtk.Entry()
-	var_combo=Gtk.ComboBoxText()
-	for variante in base.varianti:
-		var_combo.append_text(variante)
-	table.attach(name_entry,1,2,0,1,Gtk.AttachOptions(4),Gtk.AttachOptions(4))
-	table.attach(var_combo,1,2,1,2,Gtk.AttachOptions(4),Gtk.AttachOptions(4))
-	button=Gtk.Button()
-	button.set_label(_('Start game!'))
-	button.connect('pressed',inizia_partita,app,name_entry,var_combo,win_inizio)
-	button.connect('activate',inizia_partita,app,name_entry,var_combo,win_inizio)
-	table.attach(button,1,2,2,3,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
-	win_inizio.add(table)
-	name_entry.set_property("activates-default",True)
-	button.set_can_default(True)
-	button.grab_default()
-	#onlineb=Gtk.Button()
-	#onlineb.set_label(_('Online game...'))
-	#onlineb.connect('pressed',app.show_win_online)
-	#table.attach(onlineb,0,1,2,3,Gtk.AttachOptions(0),Gtk.AttachOptions(0))
-	var_combo.set_active(base.varianti.index(base.settings['variante']))
+	button.connect('pressed',inizia_partita,app)
+	button.connect('activate',inizia_partita,app)
+	var_combo.connect('changed',set_n_players)
+	var_combo.set_active_id(base.settings['variante'])
 	name_entry.set_text(base.settings['nome'])
 	win_inizio.show_all()
